fccdf752c49445743b5db9cfe91617c3a8de709f
max <max.goltzsche@gmail.com>
2013-05-29 01:27:10 +0200

    Terrain

--- OgreTerrainTutorial/src/TerrainApplication.h
+++ OgreTerrainTutorial/src/TerrainApplication.h
@@ -1,37 +1,46 @@
 /*
------------------------------------------------------------------------------
-Filename:    TerrainApplication.h
------------------------------------------------------------------------------
+ -----------------------------------------------------------------------------
+ Filename:    TerrainApplication.h
+ -----------------------------------------------------------------------------
 
-This source file is part of the
-   ___                 __    __ _ _    _ 
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
+ This source file is part of the
+ ___                 __    __ _ _    _
+ /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
  //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
-*/
+ / \_// (_| | | |  __/  \  /\  /| |   <| |
+ \___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
+ |___/
+ Tutorial Framework
+ http://www.ogre3d.org/tikiwiki/
+ -----------------------------------------------------------------------------
+ */
 #ifndef __TerrainApplication_h_
 #define __TerrainApplication_h_
 
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
 #include "BaseApplication.h"
 
-class TerrainApplication : public BaseApplication
-{
+class TerrainApplication: public BaseApplication {
 public:
-    TerrainApplication(void);
-    virtual ~TerrainApplication(void);
+	TerrainApplication(void);
+	virtual ~TerrainApplication(void);
 
-    Ogre::RenderWindow * getWindow(void) { return mWindow; }
-    Ogre::Timer * getTimer(void) { return mTimer; }
-    OIS::Mouse * getMouse(void) { return mMouse; }
-    OIS::Keyboard * getKeyboard(void) { return mKeyboard; }
 protected:
-    virtual void createScene(void);
-    Ogre::Timer *mTimer;
+	virtual void createScene(void);
+	virtual void createFrameListener(void);
+	virtual void destroyScene(void);
+	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
+private:
+	Ogre::TerrainGlobalOptions* mTerrainGlobals;
+	Ogre::TerrainGroup* mTerrainGroup;
+	OgreBites::Label* mInfoLabel;
+	bool mTerrainsImported;
+
+	void defineTerrain(long x, long y);
+	void initBlendMaps(Ogre::Terrain* terrain);
+	void configureTerrainDefaults(Ogre::Light* light);
 };
 
 #endif // #ifndef __TerrainApplication_h_
--- OgreTest/HMDApplication.cpp
+++ OgreTest/HMDApplication.cpp
@@ -186,6 +186,7 @@ void HMDApplication::initBlendMaps(Ogre::Terrain* terrain) {
 	blendMap1->update();
 }
 
+
 // start the application
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
